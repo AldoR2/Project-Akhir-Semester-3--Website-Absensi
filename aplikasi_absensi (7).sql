a3e6a9f6e9b2a373ab0cbc416beb01786ddb2d24
BimaaAchmadFillArdhi <bimaachmadardhi@gmail.com>
2024-10-03 22:27:45 +0700

    tambah foto Blob

--- aplikasi_absensi (7).sql
+++ aplikasi_absensi (7).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2024 at 02:55 PM
+-- Generation Time: Oct 03, 2024 at 05:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,22 +34,23 @@ CREATE TABLE `dosen` (
   `email` varchar(100) NOT NULL,
   `no_telp` char(13) NOT NULL,
   `alamat` varchar(255) NOT NULL,
-  `password` varchar(225) NOT NULL
+  `password` varchar(225) NOT NULL,
+  `foto` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `dosen`
 --
 
-INSERT INTO `dosen` (`nip`, `nama`, `jenis_kelamin`, `email`, `no_telp`, `alamat`, `password`) VALUES
-('1011987654321', 'Hayoo', 'laki_laki', 'hayoo@gmail.com', '0878787878787', 'Mastrip Selatan', 'hayoo'),
-('1081197654321', 'Rohim ', 'laki_laki', 'rohim@gmail.com', '082341248764', 'Mastrip Utara', 'rohim'),
-('1110987654321', 'Dwi', 'perempuan', 'dwi@gmail.com', '089847382756', 'Mastrip Timur', 'dwi'),
-('1234567891011', 'Tris', 'laki_laki', 'tris@gmail.com', '087284918392', 'Jl mastrip bagian timur', 'tris'),
-('2101198765431', 'Sugeng', 'laki_laki', 'sugeng@gmail.com', '087798987463', 'Mastrip 5 ', 'sugeng seleb'),
-('6101198754321', 'Imam', 'laki_laki', 'imam@gmail.com', '0822374618729', 'Batu raden', 'imam'),
-('7101198654321', 'saroh', 'perempuan', 'sarohcute@gmail.com', '087739818728', 'Mastrip utara titik', 'saroh cute'),
-('9101187654321', 'Nis', 'perempuan', 'nis@gmail.com', '0876778463271', 'Mastrip Barat', 'nis');
+INSERT INTO `dosen` (`nip`, `nama`, `jenis_kelamin`, `email`, `no_telp`, `alamat`, `password`, `foto`) VALUES
+('1011987654321', 'Hayoo', 'laki_laki', 'hayoo@gmail.com', '0878787878787', 'Mastrip Selatan', 'hayoo', ''),
+('1081197654321', 'Rohim ', 'laki_laki', 'rohim@gmail.com', '082341248764', 'Mastrip Utara', 'rohim', ''),
+('1110987654321', 'Dwi', 'perempuan', 'dwi@gmail.com', '089847382756', 'Mastrip Timur', 'dwi', ''),
+('1234567891011', 'Tris', 'laki_laki', 'tris@gmail.com', '087284918392', 'Jl mastrip bagian timur', 'tris', ''),
+('2101198765431', 'Sugeng', 'laki_laki', 'sugeng@gmail.com', '087798987463', 'Mastrip 5 ', 'sugeng seleb', ''),
+('6101198754321', 'Imam', 'laki_laki', 'imam@gmail.com', '0822374618729', 'Batu raden', 'imam', ''),
+('7101198654321', 'saroh', 'perempuan', 'sarohcute@gmail.com', '087739818728', 'Mastrip utara titik', 'saroh cute', ''),
+('9101187654321', 'Nis', 'perempuan', 'nis@gmail.com', '0876778463271', 'Mastrip Barat', 'nis', '');
 
 --
 -- Triggers `dosen`
@@ -105,7 +106,6 @@ CREATE TABLE `log_mahasiswa` (
   `waktu` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `nim` varchar(20) NOT NULL,
   `nama` varchar(200) NOT NULL,
-  `golongan` varchar(3) NOT NULL,
   `password` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -113,54 +113,54 @@ CREATE TABLE `log_mahasiswa` (
 -- Dumping data for table `log_mahasiswa`
 --
 
-INSERT INTO `log_mahasiswa` (`id_log`, `waktu`, `nim`, `nama`, `golongan`, `password`) VALUES
-(1, '2024-09-19 15:30:33', 'E4123123', 'Bimaa Penegak kebenaran', 'c', '12321231'),
-(2, '2024-09-25 14:07:04', 'E4123123', 'Bimaa Penegak kebenaran', 'c', '12321231'),
-(3, '2024-09-25 14:07:15', 'E4123123', 'Bimaa Penegak kebenaran', 'c', '12321231'),
-(4, '2024-09-25 14:14:56', 'E4123123', 'Bimaa Penegak kebenaran', 'c', '12321231'),
-(5, '2024-09-25 14:15:14', 'E4123123', 'Bimaa Penegak kebenaran', 'c', '12321231'),
-(6, '2024-09-25 14:16:49', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(7, '2024-09-25 14:44:34', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(8, '2024-09-25 14:45:34', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(9, '2024-09-25 14:46:34', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(10, '2024-09-25 14:47:34', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(11, '2024-09-25 14:48:34', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(12, '2024-09-25 14:48:34', 'E4123412', 'Udin sedunia', '', '1234123'),
-(13, '2024-09-25 14:49:34', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(14, '2024-09-25 14:49:34', 'E4123412', 'Udin sedunia', '', '1234123'),
-(15, '2024-09-25 14:49:51', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(16, '2024-09-25 14:49:51', 'E4123412', 'Udin sedunia', '', '1234123'),
-(17, '2024-09-25 14:50:51', 'E1234123', '', '', ''),
-(18, '2024-09-25 14:50:51', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(19, '2024-09-25 14:50:51', 'E4123412', 'Udin sedunia', '', '1234123'),
-(20, '2024-09-25 14:53:00', 'E1234123', '', '', ''),
-(21, '2024-09-25 14:53:00', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(22, '2024-09-25 14:53:00', 'E4123412', 'Udin sedunia', '', '1234123'),
-(23, '2024-09-25 14:54:00', 'E1234123', '', '', ''),
-(24, '2024-09-25 14:54:00', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(25, '2024-09-25 14:54:00', 'E4123412', 'Udin sedunia', '', '1234123'),
-(26, '2024-09-25 14:55:00', 'E1234123', '', '', ''),
-(27, '2024-09-25 14:55:00', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(28, '2024-09-25 14:55:00', 'E4123412', 'Udin sedunia', '', '1234123'),
-(29, '2024-09-25 14:55:00', 'F51235132', '', '', ''),
-(30, '2024-09-25 15:02:59', 'D51325123', 'Jahenuri', '', ''),
-(31, '2024-09-25 15:02:59', 'E1234123', '', '', ''),
-(32, '2024-09-25 15:02:59', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(33, '2024-09-25 15:02:59', 'E4123412', 'Udin sedunia', '', '1234123'),
-(34, '2024-09-25 15:02:59', 'F51235132', '', '', ''),
-(35, '2024-09-25 15:02:59', 'J51325132', 'Munip', '', ''),
-(36, '2024-09-25 15:03:59', 'D51325123', 'Jahenuri', '', ''),
-(37, '2024-09-25 15:03:59', 'E1234123', '', '', ''),
-(38, '2024-09-25 15:03:59', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(39, '2024-09-25 15:03:59', 'E4123412', 'Udin sedunia', '', '1234123'),
-(40, '2024-09-25 15:03:59', 'F51235132', '', '', ''),
-(41, '2024-09-25 15:03:59', 'J51325132', 'Munip', '', ''),
-(42, '2024-09-25 15:04:59', 'D51325123', 'Jahenuri', '', ''),
-(43, '2024-09-25 15:04:59', 'E1234123', '', '', ''),
-(44, '2024-09-25 15:04:59', 'E4123123', 'Bimaa Penegak kebenaran', '', '12321231'),
-(45, '2024-09-25 15:04:59', 'E4123412', 'Udin sedunia', '', '1234123'),
-(46, '2024-09-25 15:04:59', 'F51235132', '', '', ''),
-(47, '2024-09-25 15:04:59', 'J51325132', 'Munip', '', '');
+INSERT INTO `log_mahasiswa` (`id_log`, `waktu`, `nim`, `nama`, `password`) VALUES
+(1, '2024-09-19 15:30:33', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(2, '2024-09-25 14:07:04', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(3, '2024-09-25 14:07:15', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(4, '2024-09-25 14:14:56', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(5, '2024-09-25 14:15:14', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(6, '2024-09-25 14:16:49', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(7, '2024-09-25 14:44:34', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(8, '2024-09-25 14:45:34', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(9, '2024-09-25 14:46:34', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(10, '2024-09-25 14:47:34', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(11, '2024-09-25 14:48:34', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(12, '2024-09-25 14:48:34', 'E4123412', 'Udin sedunia', '1234123'),
+(13, '2024-09-25 14:49:34', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(14, '2024-09-25 14:49:34', 'E4123412', 'Udin sedunia', '1234123'),
+(15, '2024-09-25 14:49:51', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(16, '2024-09-25 14:49:51', 'E4123412', 'Udin sedunia', '1234123'),
+(17, '2024-09-25 14:50:51', 'E1234123', '', ''),
+(18, '2024-09-25 14:50:51', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(19, '2024-09-25 14:50:51', 'E4123412', 'Udin sedunia', '1234123'),
+(20, '2024-09-25 14:53:00', 'E1234123', '', ''),
+(21, '2024-09-25 14:53:00', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(22, '2024-09-25 14:53:00', 'E4123412', 'Udin sedunia', '1234123'),
+(23, '2024-09-25 14:54:00', 'E1234123', '', ''),
+(24, '2024-09-25 14:54:00', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(25, '2024-09-25 14:54:00', 'E4123412', 'Udin sedunia', '1234123'),
+(26, '2024-09-25 14:55:00', 'E1234123', '', ''),
+(27, '2024-09-25 14:55:00', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(28, '2024-09-25 14:55:00', 'E4123412', 'Udin sedunia', '1234123'),
+(29, '2024-09-25 14:55:00', 'F51235132', '', ''),
+(30, '2024-09-25 15:02:59', 'D51325123', 'Jahenuri', ''),
+(31, '2024-09-25 15:02:59', 'E1234123', '', ''),
+(32, '2024-09-25 15:02:59', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(33, '2024-09-25 15:02:59', 'E4123412', 'Udin sedunia', '1234123'),
+(34, '2024-09-25 15:02:59', 'F51235132', '', ''),
+(35, '2024-09-25 15:02:59', 'J51325132', 'Munip', ''),
+(36, '2024-09-25 15:03:59', 'D51325123', 'Jahenuri', ''),
+(37, '2024-09-25 15:03:59', 'E1234123', '', ''),
+(38, '2024-09-25 15:03:59', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(39, '2024-09-25 15:03:59', 'E4123412', 'Udin sedunia', '1234123'),
+(40, '2024-09-25 15:03:59', 'F51235132', '', ''),
+(41, '2024-09-25 15:03:59', 'J51325132', 'Munip', ''),
+(42, '2024-09-25 15:04:59', 'D51325123', 'Jahenuri', ''),
+(43, '2024-09-25 15:04:59', 'E1234123', '', ''),
+(44, '2024-09-25 15:04:59', 'E4123123', 'Bimaa Penegak kebenaran', '12321231'),
+(45, '2024-09-25 15:04:59', 'E4123412', 'Udin sedunia', '1234123'),
+(46, '2024-09-25 15:04:59', 'F51235132', '', ''),
+(47, '2024-09-25 15:04:59', 'J51325132', 'Munip', '');
 
 -- --------------------------------------------------------
 
@@ -176,27 +176,28 @@ CREATE TABLE `mahasiswa` (
   `email` varchar(100) NOT NULL,
   `jenis_kelamin` enum('laki_laki','perempuan') NOT NULL,
   `no_telp` char(13) NOT NULL,
-  `password` varchar(225) NOT NULL
+  `password` varchar(225) NOT NULL,
+  `foto` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `nama`, `program_studi`, `semester`, `email`, `jenis_kelamin`, `no_telp`, `password`) VALUES
-('E4123123', 'Bimaa Penegak kebenaran', 'tkk', 14, '', 'laki_laki', '', '12321231'),
-('E4123321', 'Farhan', 'MIF', 3, 'farhan@gmail.com', 'laki_laki', '087309128921', 'farhan'),
-('E41234133', 'Jalaludin', 'Tkk', 3, 'jalaludin@gmail.com', 'laki_laki', '084712984289', 'jalaludin'),
-('E4123542', 'Hermawan', 'MIF', 3, 'hermawan@gmail.com', 'laki_laki', '0834123493851', 'hermawan'),
-('E4124132', 'Hania', 'MIF', 1, 'hania@gmail.com', 'perempuan', '0847298412134', 'hania'),
-('E41244323', 'Ningsih ', 'TIF', 1, 'ningsih@gmail.com', 'perempuan', '0837828471879', 'ningsih');
+INSERT INTO `mahasiswa` (`nim`, `nama`, `program_studi`, `semester`, `email`, `jenis_kelamin`, `no_telp`, `password`, `foto`) VALUES
+('E4123123', 'Bimaa Penegak kebenaran', 'tkk', 14, '', 'laki_laki', '', '12321231', ''),
+('E4123321', 'Farhan', 'MIF', 3, 'farhan@gmail.com', 'laki_laki', '087309128921', 'farhan', ''),
+('E41234133', 'Jalaludin', 'Tkk', 3, 'jalaludin@gmail.com', 'laki_laki', '084712984289', 'jalaludin', ''),
+('E4123542', 'Hermawan', 'MIF', 3, 'hermawan@gmail.com', 'laki_laki', '0834123493851', 'hermawan', ''),
+('E4124132', 'Hania', 'MIF', 1, 'hania@gmail.com', 'perempuan', '0847298412134', 'hania', ''),
+('E41244323', 'Ningsih ', 'TIF', 1, 'ningsih@gmail.com', 'perempuan', '0837828471879', 'ningsih', '');
 
 --
 -- Triggers `mahasiswa`
 --
 DELIMITER $$
 CREATE TRIGGER `perubahan_mhs` AFTER UPDATE ON `mahasiswa` FOR EACH ROW BEGIN
-INSERT INTO log_mahasiswa (waktu, nim, password, nama, golongan) VALUES (now(), new.nim, new.password, new.nama, new.golongan);
+INSERT INTO log_mahasiswa (waktu, nim, password, nama, golongan) VALUES (now(), new.nim, new.password, new.nama);
 END
 $$
 DELIMITER ;
