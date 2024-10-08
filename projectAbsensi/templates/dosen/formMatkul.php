11282a337b3783c9ea2b36e62cd3c938647f9764
Izzul27 <158866401+Izzul27@users.noreply.github.com>
2024-10-08 09:45:02 +0700

    update3

--- projectAbsensi/templates/dosen/formMatkul.php
+++ projectAbsensi/templates/dosen/formMatkul.php
@@ -82,30 +82,9 @@
          </div>
       </section>
 
-      <footer class="footer">
-         <div class="footer__container">
-            <div class="footer__section">
-               <h4>Follow Us</h4>
-               <div class="footer__socials">
-                  <a href="https://www.instagram.com" target="_blank"><i class="ri-instagram-line"></i></a>
-                  <a href="https://www.facebook.com" target="_blank"><i class="ri-facebook-circle-line"></i></a>
-                  <a href="https://www.twitter.com" target="_blank"><i class="ri-twitter-line"></i></a>
-                  <a href="https://www.youtube.com" target="_blank"><i class="ri-youtube-line"></i></a>
-               </div>
-            </div>
-   
-            <div class="footer__section">
-               <h4>Contact Us</h4>
-               <a href="https://www.google.com/maps/place/Jalan+Yulius+Usman+No.+62,+Malang,+Indonesia"><i class="ri-map-pin-line"></i> Jalan Yulius Usman No. 62, Malang, Indonesia</a>
-               <p><i class="ri-phone-line"></i> +62 341-369003</p>
-               <a href="mailto:info@stikespantiwaluya.ac.id"><i class="ri-mail-line"></i> info@stikespantiwaluya.ac.id</a>
-            </div>
-         </div>
-   
-         <div class="footer__bottom">
-            <p>Copyrigth &copy; 2024 Stikes Panti Waluya Malang .</p>
-         </div>
-      </footer>
+<?php 
+   include("../mahasiswa/footer.php");
+?>
 
       <!--=============== MAIN JS ===============-->
       <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
